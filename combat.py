ae319ef1ab2a9d9dc99acbbe1b8d5949989fe5fa
Przemek <przemyslaw.hawron@gmail.com>
2026-03-26 16:31:13 +0100

    dodałem crit chance do attack()

--- combat.py
+++ combat.py
@@ -1,4 +1,14 @@
+import random
+
 def attack(attacker, defender):
     damage = attacker["attack"]
     defender["hp"] -= damage
-    print(attacker["name"], "hits", defender["name"], "for", damage)
+
+    critChance =  random.randint(0, 6)
+
+    if critChance == 0:
+        print(attacker["name"], "hits CRITICILY", defender["name"], "for", damage*4)
+    elif critChance%2 == 0:
+        print(attacker["name"], "hits criticly", defender["name"], "for", damage * 2)
+    else:
+        print(attacker["name"], "hits", defender["name"], "for", damage)
