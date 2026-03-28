41cae27886b8ecd656806d3ce0a5a3c0ba76294a
Przemek <przemyslaw.hawron@gmail.com>
2026-03-28 10:43:17 +0100

    dodalem menu w main.py

--- main.py
+++ main.py
@@ -11,5 +11,25 @@ def main():
     else:
         print("You won!")
 
+def task_menu():
+    print("!oto listan zadan! \n 1) pokaz zadania \n 2) dodaj zadania \n 3) usun zadania 4) pokaz zadania zrobione \n 5) pokaz zadania nie zrobione \n 6) zapisz i wyjdz \n")
+
+    choice = input("\nCo chcesz zrobic? ")
+
+    if choice == "1":
+        print("Pokaz zadania!")
+    elif choice == "2":
+        print("Dodaj zadania!")
+    elif choice == "3":
+        print("USUN zadania!")
+    elif choice == "4":
+        print("Pokaz zadania zrobione!")
+    elif choice == "5":
+        print("Pokaz zadania niezrobione!")
+    elif choice == "6":
+        print("Zapisz i wyjdz!")
+    else:
+        print("wprowaddz poprawna opcje")
+        
 if __name__ == "__main__":
     main()
