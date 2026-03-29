91fbf20c1a17374e466eaf2cc0204a46cf943213
Michał Błaszczak <mblszcz1@gmail.com>
2026-03-29 14:45:25 +0200

    combat.py

--- combat.py
+++ combat.py
@@ -1,5 +1,5 @@
 import random
-
+# zad 2 - osoba 1 
 def attack(attacker, defender):
     damage = attacker["attack"]
     defender["hp"] -= damage
@@ -12,3 +12,27 @@ def attack(attacker, defender):
         print(attacker["name"], "hits criticly", defender["name"], "for", damage * 2)
     else:
         print(attacker["name"], "hits", defender["name"], "for", damage)
+
+# zad 2 - osoba 3  - zmieniona wersja 
+
+import random
+
+def attack(attacker, defender):
+    
+    baseDamage = attacker["attack"]
+    randomBonus = random.randint(0, 5)
+    damage = baseDamage + randomBonus
+
+    critChance = random.randint(0, 6)
+
+    if critChance == 0:
+        finalDamage = damage * 4
+        print(attacker["name"], "hits CRITICLY", defender["name"], "for", finalDamage)
+    elif critChance % 2 == 0:
+        finalDamage = damage * 2
+        print(attacker["name"], "hits criticly", defender["name"], "for", finalDamage)
+    else:
+        finalDamage = damage
+        print(attacker["name"], "hits", defender["name"], "for", finalDamage)
+
+    defender["hp"] -= finalDamage
